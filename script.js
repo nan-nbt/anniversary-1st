77a50d73ae43fd0168587e8ba6597c618b5342a5
Nan Nbt <bananudin29@gmail.com>
2025-05-09 16:37:00 +0700

    update source code #2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  const slides = [
+const slides = [
     { src: 'images/castorice-purple-7680x4320-22219.jpg', caption: 'Our first photo together' },
     { src: 'images/nezuko-kamado-ai-3840x2160-12515.jpg', caption: 'That rainy walk we loved' },
     { src: 'images/tanjiro-kamado-ai-3840x2160-12479.jpg', caption: 'Your smile on our wedding day' },
@@ -9,21 +9,17 @@ let index = 0;
 function showSlide() {
   const img = document.getElementById('slide');
   const caption = document.getElementById('caption');
-  img.style.opacity = 0;
 
+  img.classList.add('opacity-0');
   setTimeout(() => {
     img.src = slides[index].src;
     caption.textContent = slides[index].caption;
-    img.style.opacity = 1;
-  }, 300);
+    img.classList.remove('opacity-0');
+  }, 400);
 }
 
-function nextSlide() {
+// Auto-slide every 5 seconds
+setInterval(() => {
   index = (index + 1) % slides.length;
   showSlide();
-}
-
-function prevSlide() {
-  index = (index - 1 + slides.length) % slides.length;
-  showSlide();
-}
+}, 5000);
