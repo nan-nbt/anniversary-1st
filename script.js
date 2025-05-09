6ea4eff00ef798c97e2868347914af0268bff0be
Nan Nbt <bananudin29@gmail.com>
2025-05-09 09:13:42 +0700

    update images source

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const slides = [
-    { src: 'images/photo1.jpg', caption: 'Our first photo together' },
-    { src: 'images/photo2.jpg', caption: 'That rainy walk we loved' },
-    { src: 'images/photo3.jpg', caption: 'Your smile on our wedding day' },
-    { src: 'images/photo4.jpg', caption: 'One year later — stronger than ever 💖' }
+    { src: 'images/castorice-purple-7680x4320-22219.jpg', caption: 'Our first photo together' },
+    { src: 'images/nezuko-kamado-ai-3840x2160-12515.jpg', caption: 'That rainy walk we loved' },
+    { src: 'images/tanjiro-kamado-ai-3840x2160-12479.jpg', caption: 'Your smile on our wedding day' },
+    // { src: 'images/photo4.jpg', caption: 'One year later — stronger than ever 💖' }
   ];
   
   let index = 0;
